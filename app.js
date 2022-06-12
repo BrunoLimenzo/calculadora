bb0f6a38554852478b0406a48a7c92bbeca6041d
BrunoLimenzo <brunolimenzo0@gmail.com>
2022-06-12 12:37:49 -0300

    Refatoração do código

---
+++ app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+   const items =  $('li');
+   for(let li of items){
+    if(li.id === ""){
+    li.addEventListener("click",function(){addValue(li.innerHTML)});
+    }else if(li.id === "botao-igual"){
+        li.addEventListener("click", writeResult);
+    }else if(li.id=== "botao-limpar"){
+        li.addEventListener("click", clearResult);
+    }
+   }
+})
+
+function addValue(value){
+    const actualValue = document.getElementById("resultado").innerHTML;
+    document.getElementById("resultado").innerHTML = actualValue + value;
+}
+
+function clearResult(){
+    document.getElementById("resultado").innerHTML = "";
+}
+
+function writeResult(){
+    let val = document.getElementById("resultado").innerHTML;
+    val = val.replace(/[x]/i,"*");
+
+    if(val && canEval(val)){
+        document.getElementById("resultado").innerHTML = eval(val);
+    }else{
+        document.getElementById("resultado").innerHTML = "ERROR";
+    }
+}
+
+function canEval(val){
+    try {
+        eval(val);
+    } catch (error) {
+        return false
+    }
+
+    return true;
+}
